945cd5d4b6183af7c3a497745f0210608a9856e0
poik1118 <79559496+poik1118@users.noreply.github.com>
2021-03-05 12:09:01 +0900

    .

---
+++ FirstProject_30208/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private string name;
+    private int level = 1;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+
+    public void getInfo()
+    {
+        Debug.Log("이름 : " + name);
+        Debug.Log("레벨 : " + level);
+    }
+
+    public void levelUP()
+    {
+        this.level++;
+        Debug.Log("레벨업~!");
+    }
+}
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    void Start()
+    {
+        Player myPlayer = new Player("홍길동");
+
+        myPlayer.getInfo();
+        myPlayer.levelUP();
+        myPlayer.getInfo();
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ FirstProject_30208/Assets/VectorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//transform은 순간이동
+//Translate는 이동
+//Vector3(x, y, z)
+public class VectorScript : MonoBehaviour
+{
+    Vector3 moveVelocity = new Vector3(0.3f, 0.0f, 0.3f);
+    void Start()
+    {
+        Vector3 playerPos = new Vector3(-0.3f, 0.0f, -4.0f);
+        transform.position = playerPos;
+    }
+
+    void Update()
+    {
+        transform.Translate(moveVelocity * Time.deltaTime);
+    }
+}
