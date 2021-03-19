11f72e6148808214d0803bcf3c106eb1149e5006
poik1118 <79559496+poik1118@users.noreply.github.com>
2021-03-19 11:54:56 +0900

    .

---
+++ RulletGame/Assets/RouletteController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0; //룰렛을 돌리기위한 회전속도지정
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    //룰렛을 10만큼 회전
+        {
+            this.rotSpeed = 10.0f;
+        }      
+        if (Input.GetMouseButtonDown(2))    //룰렛을 -10만큼 회전
+        {
+            this.rotSpeed = -10.0f;
+        }
+        if (Input.GetMouseButtonDown(1))    //룰렛을 정지시킨다
+        {
+            rotSpeed = 0.0f;
+        }
+        this.rotSpeed *= 0.987f;
+
+        transform.Rotate(0, 0, this.rotSpeed);
+    }
+}
