74ced8e7cb340266d359f840031ac5633bb21fd0
poik1118 <79559496+poik1118@users.noreply.github.com>
2021-05-14 12:02:26 +0900

    20210514_30208

---
+++ BombGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float translation;
+    private float rotation;
+
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * moveSpeed;
+        rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        transform.Translate(0, 0, translation * Time.deltaTime);
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+    }
+}
